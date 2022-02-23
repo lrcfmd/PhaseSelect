6ddffaafba131d30ffa71c23d0666191468c10c0
and.vasylenko@gmail.com <and.vasylenko@gmail.com>
2022-02-23 17:38:03 +0000

    bg example

--- _bg_class.py
+++ _bg_class.py
@@ -1,19 +1,14 @@
-# Classify icsd phases 
+import os
 import sys
-import json
-import pickle
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from Phase2Vec import Phase2Vec
-from DB.parse_phases import parse_phases
-from DB.augmentation import augment
 from Atom2Vec.Atom2Vec_encoder import Atom2Vec
-from Classification.Model import * #segment_classes_kfold, create_callback, split, class_weights, embedding
+from Classification.Model import *
 from Classification.AtomicModel import Endtoend
-from PostProcess.plotting_results import *
-from stat_models import pairwise_distances_no_broadcast
+from utils import *
 
 if __name__ == '__main__':
 
@@ -21,25 +16,28 @@ if __name__ == '__main__':
  
     Tc = 4.5
     epochs = 100
-    #dirname = 'BANDGAP/Reproduce'
-    dirname = 'BANDGAP/Attention'
+    dirname = 'test'
     log = 'bg_class_log'
-#   # Load phase vectors if precalculated
-    phases = Phase2Vec('', load_phase_vectors='DB/BandGap/mpds_phases_band_gap.csvone_hot_phases.pkl')
-    print('size of onehot:', phases.dt['onehot'].values[0].shape)
-
-#    print(test.dt.head())
+   
  
-    # Training set - all phase fields (vectorized)
-    # training = phases.dt['phases'].values
+    # Training set - all phase fields
+    # Load one-hot representations if precalculated
+    if os.path.exists('DATA/mpds_band_gap.csvone_hot_phases.pkl'):
+        phases = Phase2Vec('', load_phase_vectors='DATA/mpds_band_gap.csvone_hot_phases.pkl')
+    else:
+        phases = Phase2Vec('', load_phase_vectors='DATA/mpds_band_gap.csv')
     X = np.array([i for i in phases.dt['onehot'].values])
     y = np.where(phases.dt['max energy gap'].values > Tc, 1, 0)
 
     # Test
-    #test = Phase2Vec("", load_phase_vectors='Unexplored_ternaries.pkl', maxlength=10)
+    if os.path.exists('DATA/Ternary_phase_fields.pklone_hot_phases.pkl'):
+        test = Phase2Vec("", load_phase_vectors='DATA/Ternary_phase_fields.pklone_hot_phases.pkl',maxlength=phases.maxlength)
+    else:
+        test = Phase2Vec("", load_phase_vectors='DATA/Ternary_phase_fields.pkl',maxlength=phases.maxlength)
 
-
-    for i in range(1): 
+    # Choose a size of an ensemble of models
+    ensemble_n = 1
+    for i in range(ensemble_n): 
         # =============== MODEL CLASSIFY  ================
         model, model_att = phases.classifier(X, y, epochs=epochs, dirname=f'{dirname}/weights/{i}')
         print("Getting attention from training phase fields...")
@@ -53,7 +51,6 @@ if __name__ == '__main__':
         att = att[['phases','max energy gap', 'attention']]
         att.to_pickle(f'{dirname}/bg_phases_attention.pkl')
 
-        sys.exit() 
         # =============== TEST ============================= 
         print('size of onehot:', test.dt['onehot'].values[0].shape)
         print("Predicting classification for unexplored phase fields...")
@@ -61,7 +58,7 @@ if __name__ == '__main__':
         y_test = model.predict(test_x)
         y_test = y_test[:,1] # look at the '1' class probability
      
-        dttest = test.dt.assign(probability_Tc300K=y_test)
-        dttest = dttest[['phases','probability_Tc300K']]
-        dttest = dttest.sort_values(by='probability_Tc300K', ascending=False)
-        dttest.to_csv(f'{dirname}/unexplored_ternaries_classification_run{i}.csv', index=None)
+        dttest = test.dt.assign(probability_large_bg=y_test)
+        dttest = dttest[['phases','probability_large_bg']]
+        dttest = dttest.sort_values(by='probability_large_bg', ascending=False)
+        dttest.to_csv(f'{dirname}/unexplored_ternaries_bg_classification_run{i}.csv', index=None)
